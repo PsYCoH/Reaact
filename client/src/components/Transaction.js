dce442fdecfb5e3441185ae3c3a41efe813d2338
PsYCoH <psycohmeh@gmail.com>
2020-03-05 17:14:57 +0800

    adding date

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -7,9 +7,14 @@ export const Transaction = ({ transaction }) => {
 
     const sign = transaction.amount < 0 ? '-' : '+';
 
+    const date = transaction.createdAt;
+    console.log(date);
+
     return (
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
             {transaction.text}
+            <br/>
+            {transaction.createdAt}
             <span>{sign}Php{numberWithCommas(Math.abs(transaction.amount))}</span>
             <button onClick={() => deleteTransaction(transaction._id)} className="delete-btn">x</button>
         </li>
