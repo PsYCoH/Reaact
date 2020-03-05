2f3b82ac8e98763acda7e382a1ae1d96d8276d05
PsYCoH <psycohmeh@gmail.com>
2020-03-05 17:55:11 +0800

    date format

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,20 +1,17 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
-import { numberWithCommas } from '../utils/format';
+import { numberWithCommas, formatDate } from '../utils/format';
 
 export const Transaction = ({ transaction }) => {
     const { deleteTransaction } = useContext(GlobalContext);
 
     const sign = transaction.amount < 0 ? '-' : '+';
 
-    const date = transaction.createdAt;
-    console.log(date);
-
     return (
         <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
             {transaction.text}
             <br/>
-            {transaction.createdAt}
+            {formatDate(transaction.createdAt)}
             <span>{sign}Php{numberWithCommas(Math.abs(transaction.amount))}</span>
             <button onClick={() => deleteTransaction(transaction._id)} className="delete-btn">x</button>
         </li>
--- client/src/utils/format.js
+++ client/src/utils/format.js
@@ -1,3 +1,12 @@
 export function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+export function formatDate(string) {
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    return new Date(string).toLocaleDateString([], options);
+}
